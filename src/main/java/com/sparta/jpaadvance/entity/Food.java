f775fa0477ce92a6d6e43b81abc2f8a2e6e9db77
doyoon <doyoun0527@naver.com>
2023-11-16 21:54:40 +0900

    @ManyToOne 단방향 테스트

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -15,7 +15,7 @@ public class Food {
   private String name;
   private double price;
 
-  @OneToOne
-  @JoinColumn(name = "user_id") //외래키의 주인만이 외래키를 등록하고 삭제 할 수 있다.
+  @ManyToOne
+  @JoinColumn(name = "user_id")
   private User user;
 }
